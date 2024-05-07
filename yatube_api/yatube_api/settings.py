1dfe2163f50e9663a135fd3025ffe079e3cad67d
Evgenii Stoliarov <stoliarovea@gmail.com>
2024-05-07 20:19:12 +0300

    07.05.2024-1

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -109,7 +109,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
