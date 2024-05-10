269fdc6c4766bb5f72af7d6ea3cf5afce7495f8f
Evgenii Stoliarov <stoliarovea@gmail.com>
2024-05-10 16:36:04 +0300

    10.05.2024

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,14 +4,14 @@ from rest_framework.authtoken import views as authtoken
 from . import views
 
 urlpatterns = [
-    path('api/v1/api-token-auth/', authtoken.obtain_auth_token),
-    path('api/v1/posts/', views.api_posts),
-    path('api/v1/posts/<int:pk>/', views.api_posts_detail),
-    path('api/v1/groups/', views.api_groups),
-    path('api/v1/groups/<int:pk>/', views.api_groups_detail),
-    path('api/v1/posts/<int:pk>/comments/', views.api_posts_detail_comments),
+    path('v1/api-token-auth/', authtoken.obtain_auth_token),
+    path('v1/posts/', views.api_posts),
+    path('v1/posts/<int:pk>/', views.api_posts_detail),
+    path('v1/groups/', views.api_groups),
+    path('v1/groups/<int:pk>/', views.api_groups_detail),
+    path('v1/posts/<int:pk>/comments/', views.api_posts_detail_comments),
     path(
-        'api/v1/posts/<int:post_pk>/comments/<int:comment_pk>/',
+        'v1/posts/<int:post_pk>/comments/<int:comment_pk>/',
         views.api_posts_detail_comments_detail
     ),
 ]
